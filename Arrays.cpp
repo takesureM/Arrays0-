1d3ce8b447a986761ad17ece7dc8b73c1149559e
Takesure Mamvota <tooyoungmamvota@gmail>
2020-05-26 17:39:17 +0700

    Constructor added

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
-class Arrays {
-	private:
+class Array 
+{
+public:
+	Array()
+	{
+		this->MaxSize = 20;
+		this->size = 0;
+	}
+private:
 	int MaxSize;
 	int A[20];
 	int size;
-	
-	
 };
 int main()
 {
